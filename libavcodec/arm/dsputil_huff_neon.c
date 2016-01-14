e3f310a660fca2c9a7fa8513ef7d0d99fec71d2e
Nate Weibley <nate.weibley@prioria.com>
2016-01-14 21:44:26 +0000

    Clean up and fix SIMD diff_bytes implementation

--- libavcodec/arm/dsputil_huff_neon.c
+++ libavcodec/arm/dsputil_huff_neon.c
@@ -4,37 +4,45 @@
 #include "libavcodec/dsputil.h"
 
 #include "dsputil_arm.h"
+
+#ifdef __ARM_NEON__
 #include <arm_neon.h>
+#endif
+
+void diff_bytes_neon(uint8_t *dst, uint8_t *src1, uint8_t *src2, int w);
 
 void diff_bytes_neon(uint8_t *dst, uint8_t *src1, uint8_t *src2, int w)
 {
 	long i = 0;
-	while ((long)dst  & 0xf) {
+	// Align self
+	for (i = 0; (((long)dst + i) & 0xf) && (i < w); i++) {
 		dst[i] = src1[i] - src2[i];
 	}
-	for (i = 0; i<(w/32); i++)
+	dst  += i;
+	src1 += i;
+	src2 += i;
+	w -= i;
+#ifdef __ARM_NEON__
+	for (i = 0; i < (w - 32); i += 32)
 	{
 		// load 32 pixels
-		uint8x16_t src1PixelsA = vld1q_u8(src1);
-		uint8x16_t src2PixelsA = vld1q_u8(src2);
-		uint8x16_t src1PixelsB = vld1q_u8(src1+16);
-		uint8x16_t src2PixelsB = vld1q_u8(src2+16);
+		uint8x16_t src1PixelsA = vld1q_u8(src1 + i);
+		uint8x16_t src2PixelsA = vld1q_u8(src2 + i);
+		uint8x16_t src1PixelsB = vld1q_u8(src1 + i + 16);
+		uint8x16_t src2PixelsB = vld1q_u8(src2 + i + 16);
 
 		// subtract them
 		uint8x16_t dstPixelsA = vsubq_u8(src1PixelsA, src2PixelsA);
 		uint8x16_t dstPixelsB = vsubq_u8(src1PixelsB, src2PixelsB);
 
 		// store the result
-		vst1q_u8 (dst,    dstPixelsA);
-		vst1q_u8 (dst+16, dstPixelsB);
+		vst1q_u8 (dst + i,    dstPixelsA);
+		vst1q_u8 (dst + i + 16, dstPixelsB);
 
 		// advance 32 pixels
-		src1+=32;
-		src2+=32;
-		dst+=32;
 	}
-
-	// Clean up
+#endif
+	// Tail end
 	for (; i < w; i++) {
 		dst[i] = src1[i] - src2[i];
 	}
